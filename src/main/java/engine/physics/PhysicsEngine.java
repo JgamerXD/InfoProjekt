ce8c8f7eeba48feda3dd692268288f539a1b20ac
JgamerXD <JgamerLP@gmail.com>
2015-03-02 21:39:48 +0100

    Unstellung auf java.awt Fertig

--- src/main/java/engine/physics/PhysicsEngine.java
+++ src/main/java/engine/physics/PhysicsEngine.java
@@ -11,23 +11,23 @@ import engine.entity.EntityPhysical;
  * Created by Jan on 15.02.2015.
  */
 public class PhysicsEngine {
-	private Quadtree<PhysicsObject> objects;
+//	private Quadtree<PhysicsObject> objects;
 	
 	private void update(double delta)
 	{
-		Set<PhysicsObject> set = new HashSet();
-		objects.getAll(set);
-		
-		
-		objects.get((p -> p.mass < 0), set);
-		
-		Set<PhysicsObject> collided = new HashSet();
+//		Set<PhysicsObject> set = new HashSet();
+//		objects.getAll(set);
+//
+//
+//		objects.get((p -> p.mass < 0), set);
+//
+//		Set<PhysicsObject> collided = new HashSet();
 		
 	}
 	
-	public void resolveCollisions(PhysicsObject obj)
-	{
-		
-	}
+//	public void resolveCollisions(PhysicsObject obj)
+//	{
+//
+//	}
 	
 }
