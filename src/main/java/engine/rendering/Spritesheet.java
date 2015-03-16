895f4b88b7b638a04dd2269c731e44f8d1b1fa4f
JgamerXD <JgamerLP@gmail.com>
2015-03-16 21:33:13 +0100

    Fix: nicht quadratische Spritesheets

--- src/main/java/engine/rendering/Spritesheet.java
+++ src/main/java/engine/rendering/Spritesheet.java
@@ -9,7 +9,7 @@ public class Spritesheet {
     Image image;
     int rows,columns;
 
-    public Spritesheet(Image image, int rows, int columns) {
+    public Spritesheet(Image image, int columns, int rows) {
         this.image = image;
         this.rows = rows;
         this.columns = columns;
@@ -24,7 +24,7 @@ public class Spritesheet {
     {
         if(index%columns == 0)
             return 0;
-        return (int)((double)(index % columns) / columns * image.getHeight(null));
+        return (int)((double)(index % columns) / columns * image.getWidth(null));
     }
     private int getXEnd(int index)
     {
@@ -32,13 +32,13 @@ public class Spritesheet {
     }
     private int getYStart(int index)
     {
-        if((int)index/ rows == 0)
+        if((int)index / rows == 0)
             return 0;
         return (int)(Math.floor(index / columns) / rows * image.getHeight(null));
     }
     private int getYEnd(int index)
     {
-        return (int)(Math.floor(index / columns + 1.0) / rows * image.getWidth(null));
+        return (int)(Math.floor(index / columns + 1.0) / rows * image.getHeight(null));
     }
 
     public void draw(int i,RenderContext ctx,double xCenter, double yCenter, double width, double height)
